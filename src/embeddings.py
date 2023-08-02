9e5d1bd931eb9c9da56bb6c9c50c2be7b8074d1b
jeremy.wayland <jeremy.don.wayland@gmail.com>
2023-08-02 16:49:58 +0200

    adding phate and tSNE

--- src/embeddings.py
+++ src/embeddings.py
@@ -12,15 +12,18 @@ from numba.core.errors import NumbaDeprecationWarning, NumbaPendingDeprecationWa
 warnings.filterwarnings("ignore", category=NumbaDeprecationWarning)
 warnings.filterwarnings("ignore", category=NumbaPendingDeprecationWarning)
 warnings.filterwarnings("ignore", category=UserWarning, module="umap")
+warnings.simplefilter("ignore")
 
 os.environ["KMP_WARNINGS"] = "off"
 ######################################################################")
 from umap import UMAP
+from phate import PHATE
+from sklearn.manifold import TSNE
 
 
 def umap(data, hyperparams, seed=0, **kwargs):
-    n, d, dim, metric = hyperparams
-    umap = UMAP(
+    n, d, metric, dim = hyperparams
+    operator = UMAP(
         n_neighbors=n,
         min_dist=d,
         n_components=dim,
@@ -28,5 +31,29 @@ def umap(data, hyperparams, seed=0, **kwargs):
         init="spectral",
         random_state=seed,
     )
-    projection = umap.fit_transform(data)
+    projection = operator.fit_transform(data)
+    return projection
+
+
+def tSNE(data, hyperparams, seed=0, **kwargs):
+    perplexity, ee, dim = hyperparams
+    operator = TSNE(
+        n_components=dim,
+        perplexity=perplexity,
+        early_exaggeration=ee,
+    )
+    projection = operator.fit_transform(data)
+    return projection
+
+
+def phate(data, hyperparams, seed=0, **kwargs):
+    knn, gamma, dim = hyperparams
+    operator = PHATE(
+        n_components=dim,
+        knn=knn,
+        gamma=gamma,
+        random_state=seed,
+        verbose=0,
+    )
+    projection = operator.fit_transform(data)
     return projection
